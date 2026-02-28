ae5baaefcf014170b67fdcfdb8feb89fbff46c85
mrunal <mrunal@coalescence.tech>
2026-03-01 01:16:54 +0530

    initial commit

--- src/config/database.js
+++ src/config/database.js
@@ -5,7 +5,8 @@ const password = encodeURIComponent(process.env.DB_PASSWORD);
 const cluster = process.env.DB_CLUSTER; // e.g. cluster1.xnxexyj.mongodb.net
 const dbName = process.env.DB_NAME;     // e.g. mydatabase
 
-const uri = `mongodb+srv://${user}:${password}@${cluster}/${dbName}?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://carveurway92_db_user:admin1@3@cluster0.qr0kqzi.mongodb.net/`;
+              
 
 // const uri = 'mongodb://127.0.0.1:27017/ambedkar'; 
 
